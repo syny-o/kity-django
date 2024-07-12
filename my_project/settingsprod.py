44c213d14a16005cdd10c377a4639d044da81521
syny-o <synek.o@seznam.cz>
2024-07-12 08:49:22 +0200

    updated settingsprod

--- my_project/settingsprod.py
+++ my_project/settingsprod.py
@@ -26,12 +26,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', '')
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['itutorialy.cz',]
 
 
 # Application definition
