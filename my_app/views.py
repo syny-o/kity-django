6fde9bc768d67149a0db88a93362c1d5990eb509
syny-o <synek.o@seznam.cz>
2024-07-12 09:45:21 +0200

    added simple form + model for contact

--- my_app/views.py
+++ my_app/views.py
@@ -1,6 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+
+from .forms import ContactForm
+
 
 
 
 def home(request):
-    return render(request, 'home.html')
+
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Email sent successfully.')
+            return redirect('home')
+
+    else:
+        form = ContactForm()
+
+    context = {
+        'form': form
+    }
+
+
+    return render(request, 'home.html', context)
