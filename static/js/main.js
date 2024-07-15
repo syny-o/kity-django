6d9c22eb07e843249d3c1c1a5cafdd1bd0cce712
syny-o <synek.o@seznam.cz>
2024-07-15 19:18:19 +0200

    fixed left menu z-index + updated script for filtering photos

--- static/js/main.js
+++ static/js/main.js
@@ -104,12 +104,18 @@
         itemSelector: '.portfolio-item',
         layoutMode: 'fitRows'
     });
+
+    // Automatically filter to show only .portrait items on initial load
+    portfolioIsotope.isotope({ filter: '.portrait' });
+
     $('#portfolio-flters li').on('click', function () {
         $("#portfolio-flters li").removeClass('active');
         $(this).addClass('active');
 
         portfolioIsotope.isotope({filter: $(this).data('filter')});
     });
+
+    
 
 
     // Testimonials carousel
