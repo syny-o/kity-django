8724549428aa5baaefa9acd974fc2d68acb5eb5d
syny-o <synek.o@seznam.cz>
2024-07-15 18:18:48 +0200

    hidden url for testing

--- my_project/urls.py
+++ my_project/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('my_app.urls')),
+    path('my_app', include('my_app.urls')),
 ]
