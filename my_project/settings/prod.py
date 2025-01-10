12c1a31cca67061de85f9bff5519376a9bbf954b
syny-o <synek.o@seznam.cz>
2025-01-10 07:50:04 +0100

    added logging + updated allowed hosts

--- my_project/settings/prod.py
+++ my_project/settings/prod.py
@@ -5,12 +5,12 @@ from .base import *
 DEBUG = False
 
 
-ALLOWED_HOSTS = ['filateliestafl.eu', 'www.filateliestafl.eu']
+ALLOWED_HOSTS = ['marketasynkova.cz', 'www.marketasynkova.cz']
 
 
 
 CORS_ALLOWED_ORIGINS = [
-    "https://filateliestafl.eu",
+    "https://marketasynkova.cz",
 ]
 
 CSRF_TRUSTED_ORIGINS = [*CORS_ALLOWED_ORIGINS]
@@ -27,4 +27,22 @@ DATABASES = {
         'PORT': '',
     }
 }
+
+
+
+LOGGING = {                                                                                                                 
+            'version': 1,
+                'disable_existing_loggers': False,
+                    'handlers': {
+                                'logfile': {
+                                                'class': 'logging.FileHandler',
+                                                            'filename': '../logs/django_server.log',
+                                                                    },
+                                    },
+                        'loggers': {
+                                    'django': {
+                                                    'handlers': ['logfile'],
+                                                            },
+                                        },
+                        }
 
