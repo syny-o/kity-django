a76353e0321f97477a61f499cc2a93a5322813aa
syny-o <synek.o@seznam.cz>
2025-08-19 16:28:23 +0200

    update port from 587 to 2525 (Digital Ocean now blocking 587 port because of potential spams)

--- my_project/settings/base.py
+++ my_project/settings/base.py
@@ -134,7 +134,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = "live.smtp.mailtrap.io"
 EMAIL_USE_TLS = True
-EMAIL_PORT = '587'
+EMAIL_PORT = '2525'
 EMAIL_HOST_USER = "api"
 EMAIL_HOST_PASSWORD = '143fc6a3815c9a69da30b020d67046a7'
 
